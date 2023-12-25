c335652c0340a9c73c938dc1a1645b27dbb8e970
mashaco <mstarceva@mail.ru>
2023-12-25 18:12:30 +0300

    [задача №8,5] Создать приложение core

--- megano/megano/settings.py
+++ megano/megano/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ('django-insecure-ubfygw(x!&w@&%u_dbdvq#5$6)-'
-              'kae1pqy+kl4^08+z8^1%#c%')
+SECRET_KEY = ('django-insecure-ubfygw(x!&w@&%u_dbdvq#5$6)-kae1pqy+kl4^08+z8^1%#c%')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -88,20 +87,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.'
-                'UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.'
-                'MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.'
-                'CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.'
-                'NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
