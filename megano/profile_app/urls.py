c2dff11ad763bd23760078326c624308aa808842
igor <igorblu@mail.ru>
2024-01-15 22:33:04 +0300

    Создать приложение auth (задача № 10)

--- megano/profile_app/urls.py
+++ megano/profile_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
 
+app_name = 'profile_app'
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='profile_app/profile.html'), name='profile'),
+    path('', TemplateView.as_view(template_name='profile_app/account.html'), name='account'),
+    path('update_profile/', TemplateView.as_view(template_name='profile_app/profile.html'), name='update_profile'),
 ]
 
