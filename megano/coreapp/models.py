3d8b5ec034f5449946d44ef75d1472145a47e7e7
fan_leon <pythonrnd@gmail.com>
2024-01-21 10:59:20 +0300

    [задача 22] разработка страницы управления настройками в административном разделе

--- megano/coreapp/models.py
+++ megano/coreapp/models.py
@@ -11,3 +11,21 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
+
+class Setting(BaseModel):
+    """Модель для хранения настроек"""
+    key = models.CharField(max_length=255, unique=True, verbose_name=_('key'))
+    value = models.TextField(verbose_name=_('value'))
+    cache = models.IntegerField(default=3000, verbose_name=_('cache'))
+    debug = models.BooleanField(default=True, verbose_name=_('debug'))
+
+    class Meta:
+        verbose_name = _('Настройка')
+        verbose_name_plural = _('Настройки')
+
+    def __str__(self):
+        return self.key
+
+    def set_cache(self, new_cache_value):
+        self.cache = new_cache_value
+        self.save()
