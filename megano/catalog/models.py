4609372114d92e454be11a0d62dafd6a4ab64a1e
igor <igorblu@mail.ru>
2024-01-25 23:05:18 +0300

    Задача 7. Создание модели категорий каталога (Задача 18)

--- megano/catalog/models.py
+++ megano/catalog/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=30)
-    image = models.ImageField()
+    """
+    Модель категории товара
+    """
+    title = models.CharField(verbose_name=_('title'), max_length=30, unique=True, blank=False)
+    image = models.ImageField(
+        verbose_name=_('image'),
+        upload_to=f'categories/{title.verbose_name}/%Y/%m/%d',
+        blank=True,
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+    )
