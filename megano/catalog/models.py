cd6695b242234c7c18fbe637add58fa7b8e93ed0
igor <igorblu@mail.ru>
2024-01-26 21:10:07 +0300

    Задача 7. Создание модели категорий каталога (Задача 18)

--- megano/catalog/models.py
+++ megano/catalog/models.py
@@ -6,6 +6,10 @@ class Category(models.Model):
     """
     Модель категории товара
     """
+
+    def __str__(self):
+        return f'Модель категории {self.title}'
+
     title = models.CharField(verbose_name=_('title'), max_length=30, unique=True, blank=False)
     image = models.ImageField(
         verbose_name=_('image'),
