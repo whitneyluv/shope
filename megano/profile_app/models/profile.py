76985f88efe2ea1244ff9a04e36976fdd2353841
igor <igorblu@mail.ru>
2024-01-28 20:06:04 +0300

    Задача 10. Создание модели пользователей (Задача 21)

--- megano/profile_app/models/profile.py
+++ megano/profile_app/models/profile.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.utils.translation import gettext_lazy as _
 
 
 class Profile(models.Model):
@@ -11,5 +12,9 @@ class Profile(models.Model):
         return f'Модель профиля для пользователя {self.user}'
 
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    phone = models.CharField(max_length=20)
-    avatar = models.ImageField()
+    phone = models.CharField(_('phone number'), max_length=10, blank=True)
+    avatar = models.ImageField(
+        _('profile avatar'),
+        upload_to=f'profiles/{user.email}/%Y/%m/%d',
+        blank=True
+    )
