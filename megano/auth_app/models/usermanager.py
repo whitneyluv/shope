7afb88e8cf5df8f19ecf9a4b68eb9d4d895fa4c8
igor <igorblu@mail.ru>
2024-02-01 20:35:44 +0300

    Поправил UserManager

--- megano/auth_app/models/usermanager.py
+++ megano/auth_app/models/usermanager.py
@@ -25,7 +25,6 @@ class NewUserManager(BaseUserManager):
         """
         Функция обертка создания обычного пользователя, без привилегий суперпользователя или персонала (к примеру админ)
         """
-        extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
         return self._create_user(email, password, **extra_fields)
 
@@ -33,11 +32,8 @@ class NewUserManager(BaseUserManager):
         """
         Функция обертка создания пользователя с привилегиями суперпользователя
         """
-        extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
