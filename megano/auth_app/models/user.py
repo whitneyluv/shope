7c9eb6f25f2042873a173dd5c236d30a76c425d5
igor <igorblu@mail.ru>
2024-02-02 19:42:57 +0300

    Исправлена модель юзера

--- megano/auth_app/models/user.py
+++ megano/auth_app/models/user.py
@@ -1,10 +1,10 @@
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 from django.db import models
 from .usermanager import NewUserManager
 
 
-class User(AbstractBaseUser, PermissionsMixin):
+class User(AbstractUser):
     """
     Новая модель пользователя
     """
--- megano/auth_app/models/usermanager.py
+++ megano/auth_app/models/usermanager.py
@@ -4,7 +4,7 @@ from django.contrib.auth.base_user import BaseUserManager
 class NewUserManager(BaseUserManager):
     use_in_migrations = True
 
-    def _create_user(self, email, password, **extra_fields):
+    def _create_user(self, email, username, password, **extra_fields):
         """
         Функция для внутреннего пользователя.
         Создаёт и сохраняет пользователя с переданным email и паролем в базу данных
@@ -16,25 +16,29 @@ class NewUserManager(BaseUserManager):
             raise ValueError('Пароль должен быть установлен')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=email, username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, email, password, username=None, **extra_fields):
         """
-        Функция обертка создания обычного пользователя, без привилегий суперпользователя или персонала (к примеру админ)
+        Функция обертка создания обычного пользователя, без привилегий суперпользователя или персонала
         """
+        extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(email, username, password, **extra_fields)
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, email, password, username=None, **extra_fields):
         """
         Функция обертка создания пользователя с привилегиями суперпользователя
         """
+        extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(email, username, password, **extra_fields)
