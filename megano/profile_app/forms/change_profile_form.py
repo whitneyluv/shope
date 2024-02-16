cde6a951a546f1ba8c0a7247d7c844877078a2ec
igor <igorblu@mail.ru>
2024-02-16 20:07:11 +0300

    Задача 25.1. Разработка страницы профиля (Задача 51)

--- megano/profile_app/forms/change_profile_form.py
+++ megano/profile_app/forms/change_profile_form.py
@@ -7,7 +7,7 @@ class ProfileChangeForm(forms.ModelForm):
     avatar = forms.ImageField(widget=forms.FileInput(attrs={'class': 'Profile-file form-input'}))
     username = forms.CharField(max_length=150, label='Имя пользователя')
     email = forms.EmailField()
-    phone = forms.CharField(label='Телефон')
+    phone = forms.CharField(label='Телефон', widget=forms.NumberInput(attrs={'type': 'tel', 'class': 'data-tel-input'}))
     password1 = forms.CharField(widget=forms.PasswordInput(), label='Пароль')
     password2 = forms.CharField(widget=forms.PasswordInput(), label='Подтверждение пароля')
 
