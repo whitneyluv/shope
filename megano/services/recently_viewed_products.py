01eca08e8092ed58ae7cc9faa52324d7761c5ddc
ms <ff9147782@gmail.com>
2024-02-20 22:27:52 +0300

    fixed RecentlyViewedProducts

--- megano/services/recently_viewed_products.py
+++ megano/services/recently_viewed_products.py
@@ -5,7 +5,7 @@ class RecentlyViewedProducts:
     """Класс для реализации методов работы с недавно просмотренными товарами"""
 
     def __init__(self, user_id: int) -> None:
-        self.user = get_user_model().objects.get(user=user_id)
+        self.user = get_user_model().objects.get(id=user_id)
 
     def add(self, product_id: int) -> None:
         """Метод добавления товара в список недавно просмотренных"""
