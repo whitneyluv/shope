0e1b92dd7f1e3bc40fb829ea497f540b6bae6fe5
Maria Startseva <mstarceva@mail.ru>
2024-02-27 19:07:34 +0300

    Update admin.py

--- megano/catalog/admin.py
+++ megano/catalog/admin.py
@@ -4,16 +4,16 @@ from .models import Product, Category, Price
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    pass
-    # actions = ['clear_cache']
-    #
-    # # def clear_cache(self, request, queryset):
-    # #     for product in queryset:
-    # #         cache_key = f'catalog_{product.category_id}_products'
-    # #         # cache.delete(cache_key)
-    # #     self.message_user(request, f'Cache cleared for {queryset.count()} products.')
-    #
-    # clear_cache.short_description = "Clear cache for selected products"
+    
+    actions = ['clear_cache']
+    
+        def clear_cache(self, request, queryset):
+            for product in queryset:
+                cache_key = f'catalog_{product.category_id}_products'
+                cache.delete(cache_key)
+            self.message_user(request, f'Cache cleared for {queryset.count()} products.')
+    
+    clear_cache.short_description = "Clear cache for selected products"
 
 
 @admin.register(Category)
