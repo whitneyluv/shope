a39fc9cb33d167632af98334688ce91f428cdb9b
mashaco <mstarceva@mail.ru>
2024-03-06 14:29:25 +0300

    Ссылки на главную страницу,если банеров нет, то блок не отобразиться вообще

--- megano/coreapp/utils/injector.py
+++ megano/coreapp/utils/injector.py
@@ -1,15 +1,15 @@
 import inject
 
-from megano.auth_app.interfaces.auth_interface import IAuth
-from megano.auth_app.repositories.auth_repositories import AuthRepository
-from megano.catalog.interfaces.catalog_interface import ICatalogRepository
-from megano.catalog.repositories.catalog_repositories import CatalogRepository
-from megano.profile_app.interfaces import IProfile
-from megano.profile_app.repositories import ProfileRepository
-from megano.coreapp.interfaces.core_interface import ICore
-from megano.coreapp.repositories.core_repositories import CoreRepository
-from megano.discounts_app.interfaces.discounts_interface import IDiscounts
-from megano.discounts_app.repositories.discounts_repositories import DiscountsRepository
+from auth_app.interfaces.auth_interface import IAuth
+from auth_app.repositories.auth_repositories import AuthRepository
+from catalog.interfaces.catalog_interface import ICatalogRepository
+from catalog.repositories.catalog_repositories import CatalogRepository
+from profile_app.interfaces import IProfile
+from profile_app.repositories import ProfileRepository
+from coreapp.interfaces.core_interface import ICore
+from coreapp.repositories.core_repositories import CoreRepository
+from discounts_app.interfaces.discounts_interface import IDiscounts
+from discounts_app.repositories.discounts_repositories import DiscountsRepository
 
 BINDS = (
     (IAuth, AuthRepository),
