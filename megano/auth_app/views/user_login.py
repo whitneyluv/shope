536b6139ac2e3f805aeb838730ab94dffddcf602
igor <igorblu@mail.ru>
2024-03-11 21:38:58 +0300

    Задача 20.5. Интеграция с сервисом добавления товара в корзину для неавторезированного пользователя(Задача 36)

--- megano/auth_app/views/user_login.py
+++ megano/auth_app/views/user_login.py
@@ -1,8 +1,9 @@
 import inject
 from django.contrib.auth.views import LoginView
-from django.shortcuts import redirect
 from django.contrib.auth import login as auth_login
 from profile_app.interfaces import IProfile
+from ..utils import add_to_cart_from_session
+from django.http import HttpResponseRedirect
 
 
 class UserLoginView(LoginView):
@@ -16,9 +17,10 @@ class UserLoginView(LoginView):
     def form_valid(self, form):
         """Security check complete. Log the user in."""
         auth_login(self.request, form.get_user())
-        profile = self._profile.get_profile_by_user_id(self.request.user.pk)
-        return redirect('profile_app:profile', pk=profile.pk)
+        return HttpResponseRedirect(self.get_success_url())
 
     def get_success_url(self):
+        if self.request.session.get('cart'):
+            add_to_cart_from_session(self.request)
         profile = self._profile.get_profile_by_user_id(self.request.user.pk)
         return f'/profile/{profile.pk}/'
