f5c1ee96efdc219f06f5b6e6e27e55d8810f5e58
fan_leon <pythonrnd@gmail.com>
2024-03-24 19:28:14 +0300

    fixtures

--- megano/coreapp/management/commands/apply_fixture.py
+++ megano/coreapp/management/commands/apply_fixture.py
@@ -7,27 +7,28 @@ class Command(BaseCommand):
     """
     Applies all fixtures to the database
     """
+
     def handle(self, *args, **options):
         self.stdout.write("=" * 40 +
                           "\nApply all fixtures to the database\n\n")
         total_error = list()
         paths = [
             # сначала юзер нужен и так далее
-            #loaddata - загрузить
-            #dumpdata выгрузить - python -Xutf8 manage.py  dumpdata coreapp.banner --output data.json
-            #приминять фикстуры  python manage.py apply_fixture
+            # loaddata - загрузить
+            # dumpdata выгрузить - python -Xutf8 manage.py  dumpdata coreapp.banner --output data.json
+            # приминять фикстуры  python manage.py apply_fixture
 
-            "auth_app/fixtures/user_fixtures.json",
             "auth_app/fixtures/group_fixtures.json",
-            "auth_app/fixtures/user_groups_fixtures.json",
             "auth_app/fixtures/permissions.json",
+            "auth_app/fixtures/user_fixtures.json",
+            "auth_app/fixtures/user_groups_fixtures.json",
             "profile_app/fixtures/profile_fixtures.json",
-            "profile_app/fixtures/seller_fixtures.json",
             "catalog/fixtures/category.json",
             "catalog/fixtures/characteristic.json",
             "catalog/fixtures/product.json",
             "catalog/fixtures/productimage.json",
             "catalog/fixtures/productcharacteristic.json",
+            "profile_app/fixtures/seller_fixtures.json",
             "catalog/fixtures/price.json",
         ]
         for path in paths:
