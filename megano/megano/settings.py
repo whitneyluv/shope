17366a1cc834332fcdd31eb35490ccd0a13a0067
Максим Семенюк <expmax85@gmail.com>
2024-04-22 19:16:11 +0300

    Update settings.py

--- megano/megano/settings.py
+++ megano/megano/settings.py
@@ -168,8 +168,7 @@ TIME_OUT_BANNERS = int(os.getenv("TIME_OUT_BANNERS"))
 
 CACHES = {
    "default": {
-       "BACKEND": "django.core.cache.backends.memcached.PyMemcacheCache",
-       "LOCATION": "127.0.0.1:11211",
+       "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
    }
 }
 CACHE_MIDDLEWARE_SECONDS = int(os.getenv("CACHE_MIDDLEWARE_SECONDS"))
